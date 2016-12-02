2ac1912737f599ac6b20fa4f6f057809777e9b05
mknexen <mknexen@users.noreply.github.com>
2016-12-02 12:40:31 +0100

    update Firefox User-Agent

--- cron.php
+++ cron.php
@@ -342,12 +342,13 @@ class CronController {
 				CURLOPT_IPRESOLVE => CURL_IPRESOLVE_V4,
 				CURLOPT_ENCODING => 'gzip',
 				CURLOPT_HTTPHEADER => array(
-					'User-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64; rv:24.0) Gecko/20100101 Firefox/24.0',
-					'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-					'Accept-Language: fr,fr-fr;q=0.8,en-us;q=0.5,en;q=0.3',
-					'Accept-Encoding: gzip, deflate',
-					'DNT: 1',
-					'Connection: keep-alive',
+	                'User-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0',
+	                'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	                'Accept-Language: fr,fr-FR;q=0.8,en-US;q=0.5,en;q=0.3',
+	                'Accept-Encoding: gzip, deflate',
+	                'DNT: 1',
+	                'Connection: keep-alive',
+	                'Upgrade-Insecure-Requests: 1',
 				),
 			);
 
